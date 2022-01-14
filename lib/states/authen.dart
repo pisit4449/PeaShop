0e71f000724eaf88a818440fac38fc2f3098f7d9
pea <pea4449@gmail.com>
2022-01-14 20:30:32 +0700

    Upload Two

---
+++ lib/states/authen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:peashop/utility/my_constant.dart';
+import 'package:peashop/widget/show_image.dart';
+import 'package:peashop/widget/show_title.dart';
+
+class Authen extends StatefulWidget {
+  const Authen({Key? key}) : super(key: key);
+
+  @override
+  _AuthenState createState() => _AuthenState();
+}
+
+class _AuthenState extends State<Authen> {
+  @override
+  Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          children: [
+            buildImage(size),
+            buildTitle(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Row buildTitle() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ShowTitle(
+                title: MyConstant.appName,
+                textStyle: MyConstant().h1Style(),
+              ),
+            ],
+          );
+  }
+
+  Row buildImage(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: size * 0.6,
+          child: ShowImage(path: MyConstant.image4),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/utility/my_constant.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyConstant {
+// genarol
+  static String appName = 'Pea Shop';
+
+// Route
+  static String routeAuthen = '/authen';
+  static String routeCreateAccount = '/createAccount';
+  static String routeBuyerService = '/buyerService';
+  static String routeSellerService = '/sellerService';
+  static String routeriderService = '/riderService';
+
+// Images
+  static String image1 = 'images/image1.png';
+  static String image2 = 'images/image2.png';
+  static String image3 = 'images/image3.png';
+  static String image4 = 'images/image4.png';
+  static String image5 = 'images/image5.png';
+
+// Colors
+  static Color primary = Color(0xff8100c1);
+  static Color dark = Color(0xff4c008f);
+  static Color light = Color(0xffb648f5);
+
+// Style
+  TextStyle h1Style() => TextStyle(
+        fontSize: 24,
+        color: dark,
+        fontWeight: FontWeight.bold,
+      );
+  TextStyle h2Style() => TextStyle(
+        fontSize: 18,
+        color: dark,
+        fontWeight: FontWeight.w700,
+      );
+  TextStyle h3Style() => TextStyle(
+        fontSize: 14,
+        color: dark,
+        fontWeight: FontWeight.normal,
+      );
+}
